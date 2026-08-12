0e10f9cd09da21fccf221c5c1ec1a43ae6af7868
fazilmuhammad <fazziilmuhammad@gmail.com>
2026-08-12 08:54:49 +0700

    feat: add users table migration and update product codes in seed script

---
+++ prisma/migrations/20260812015053_add_users/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(50) NULL,
+    `position` VARCHAR(100) NULL,
+    `avatar` VARCHAR(255) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_username_key`(`username`),
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
